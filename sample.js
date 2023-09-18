16c7290284ba2304ce38995a3890bb66ab85411a
faixanmalik <faixan.malik554@gmail.com>
2023-09-18 15:13:01 +0500

    delete extra files

--- sample.js
+++
@@ -1,6 +0,0 @@
-const [filteredInvoices, setFilteredInvoices] = useState([])
-
-let filteredInvoices = dbVouchers.filter((item)=>{
-    return item.userEmail === userEmail;
-  })
-  setFilteredInvoices(filteredInvoices)
