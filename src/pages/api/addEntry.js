100b43df2c61739573171a04cf0d4b372d4a803d
faixanmalik <faixan.malik554@gmail.com>
2023-09-22 16:07:15 +0500

    userEmail added in all form apis

--- src/pages/api/addEntry.js
+++ src/pages/api/addEntry.js
@@ -24,20 +24,20 @@ export default async function handler(req, res) {
         const { path } = req.body;
 
         if( path === 'employees'){
-            const { name, fatherName, dob, email, cnic,  phoneNo, citizenship, gender, maritalStatus, designation, department, workShift, workHour, employmentMode, payPolicy, basicPay, paymentMode, status, hireDate, siteName, joiningDate, country, streetAddress, city, state, zip, row, importEntries  } = req.body;
+            const { userEmail, name, fatherName, dob, email, cnic,  phoneNo, citizenship, gender, maritalStatus, designation, department, workShift, workHour, employmentMode, payPolicy, basicPay, paymentMode, status, hireDate, siteName, joiningDate, country, streetAddress, city, state, zip, row, importEntries  } = req.body;
             if(importEntries){
                 await Employees.insertMany(row);
                 res.status(200).json({ success: true, message: "Entry Added!" }) 
             }
             else{
-                let newEntry = new Employees( { name, fatherName, dob, email, cnic,  phoneNo, citizenship, gender, maritalStatus, designation, department, workShift, workHour, employmentMode, payPolicy, basicPay, paymentMode, status, hireDate, siteName, joiningDate, country, streetAddress, city, state, zip } );
+                let newEntry = new Employees( { userEmail, name, fatherName, dob, email, cnic,  phoneNo, citizenship, gender, maritalStatus, designation, department, workShift, workHour, employmentMode, payPolicy, basicPay, paymentMode, status, hireDate, siteName, joiningDate, country, streetAddress, city, state, zip } );
                 await newEntry.save();
                 res.status(200).json({ success: true, message: "Entry Added!" }) 
             }
 
         }
         else if( path === 'chartsOfAccounts'){
-            const { accountCode, accountName, account, balance , asof, desc, subAccount, row, importEntries  } = req.body;
+            const { userEmail, accountCode, accountName, account, balance , asof, desc, subAccount, row, importEntries  } = req.body;
 
             let dbChart = await Charts.findOne({accountCode})
             if(dbChart){
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
                     res.status(200).json({ success: true, message: "Entry Added!" }) 
                 }
                 else{
-                    let newCharts = new Charts( { account, accountCode, accountName, balance , asof, desc, subAccount } );
+                    let newCharts = new Charts( { userEmail, account, accountCode, accountName, balance , asof, desc, subAccount } );
                     await newCharts.save();
                     res.status(200).json({ success: true, message: "Entry Added!" }) 
                 }
@@ -69,43 +69,43 @@ export default async function handler(req, res) {
             }
         }
         else if( path === 'contactList'){
-            const { name, type, email, phoneNo, country, streetAddress, city, state, zip, taxRigNo, paymentMethod, terms , openingBalance, date, row, importEntries } = req.body;
+            const { userEmail, name, type, email, phoneNo, country, streetAddress, city, state, zip, taxRigNo, paymentMethod, terms , openingBalance, date, row, importEntries } = req.body;
 
             if(importEntries){
                 await Contact.insertMany(row);
                 res.status(200).json({ success: true, message: "Entry Added !" }) 
             }
             else{
-                let newContact = new Contact( { name, type, email, phoneNo, country, streetAddress, city, state, zip, taxRigNo, paymentMethod, terms , openingBalance, date } );
+                let newContact = new Contact( { userEmail, name, type, email, phoneNo, country, streetAddress, city, state, zip, taxRigNo, paymentMethod, terms , openingBalance, date } );
                 await newContact.save();
                 res.status(200).json({ success: true, message: "Entry Added !" }) 
             }
 
         }
         else if( path === 'productAndServices'){
-            const { code, name, linkAccount, desc, row, importEntries  } = req.body;
+            const { userEmail, code, name, linkAccount, desc, row, importEntries  } = req.body;
 
             if(importEntries){
                 Product.insertMany(row);
                 res.status(200).json({ success: true, message: "Entry Added !" }) 
             }
             else{
-                let newProduct = new Product( { code, name, linkAccount, desc  } );
+                let newProduct = new Product( { userEmail, code, name, linkAccount, desc  } );
                 await newProduct.save();
                 res.status(200).json({ success: true, message: "Entry Added !"}) 
             }
         }
         else if( path === 'addRole'){
-            const { roleName, roleDesc } = req.body;
+            const { userEmail, roleName, roleDesc } = req.body;
             
-            let newEntry = new Role( { roleName, roleDesc } );
+            let newEntry = new Role( { userEmail, roleName, roleDesc } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added!" }) 
         }
         else if( path === 'PaymentMethod'){
-            const { paymentType, chartsOfAccount } = req.body;
+            const { userEmail, paymentType, chartsOfAccount } = req.body;
             
-            let newEntry = new PaymentMethod( { paymentType, chartsOfAccount } );
+            let newEntry = new PaymentMethod( { userEmail, paymentType, chartsOfAccount } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added!" }) 
         }
@@ -115,27 +115,27 @@ export default async function handler(req, res) {
 
         // Credit Sales Invoice
         else if( path === 'CreditSalesInvoice'){
-            const { phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
 
-            let newEntry = new CreditSalesInvoice( { phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
+            let newEntry = new CreditSalesInvoice( { userEmail, phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added !" }) 
         }
 
         // Purchase Invoice
         else if( path === 'PurchaseInvoice'){
-            const { phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
 
-            let newEntry = new PurchaseInvoice( { phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
+            let newEntry = new PurchaseInvoice( { userEmail, phoneNo, email, discount, billStatus, amountPaid, amountReceived, city, address, reference, dueDate, inputList, name,  memo, journalDate, billNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added !" }) 
         }
         
         // Tax Rate
         else if( path === 'TaxRate'){
-            const { name, taxRate, chartsOfAccount } = req.body;
+            const { userEmail, name, taxRate, chartsOfAccount } = req.body;
             
-            let newEntry = new TaxRate( { name, taxRate, chartsOfAccount } );
+            let newEntry = new TaxRate( { userEmail, name, taxRate, chartsOfAccount } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added!" }) 
         }
@@ -151,9 +151,9 @@ export default async function handler(req, res) {
 
         // Expenses Invoice
         else if( path === 'Expenses'){
-            const { phoneNo, email, city, fromAccount, paidBy, project, dueDate, inputList, name,  memo, journalDate, journalNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, city, fromAccount, paidBy, project, dueDate, inputList, name,  memo, journalDate, journalNo, fullAmount, fullTax, totalAmount, attachment, path, importEntries, row } = req.body;
 
-            let newEntry = new Expenses( { phoneNo, email, city, fromAccount, paidBy, project, dueDate, inputList, name,  memo, journalDate, journalNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
+            let newEntry = new Expenses( { userEmail, phoneNo, email, city, fromAccount, paidBy, project, dueDate, inputList, name,  memo, journalDate, journalNo, fullAmount, fullTax, totalAmount, attachment, type:path } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added !" }) 
         }
@@ -165,7 +165,7 @@ export default async function handler(req, res) {
 
         // Payment Voucher Invoice
         else if( path === 'PaymentVoucher'){
-            const { phoneNo, email, city, reference, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, city, reference, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, path, importEntries, row } = req.body;
 
         
             for (const newItem of inputList) {
@@ -189,7 +189,7 @@ export default async function handler(req, res) {
                 }
             }
             
-            let newEntry = new PaymentVoucher( { phoneNo, email, city, reference, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, type:path } );
+            let newEntry = new PaymentVoucher( { userEmail, phoneNo, email, city, reference, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, type:path } );
             await newEntry.save();
 
             res.status(200).json({ success: true, message: "Entry Added !" }) 
@@ -198,7 +198,7 @@ export default async function handler(req, res) {
 
         // Receipt Voucher Invoice
         else if( path === 'ReceiptVoucher'){
-            const { phoneNo, email, city, reference, amount, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, city, reference, amount, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, path, importEntries, row } = req.body;
 
         
             for (const newItem of inputList) {
@@ -222,7 +222,7 @@ export default async function handler(req, res) {
                 }
             }
             
-            let newEntry = new ReceiptVoucher( { phoneNo, email, city, reference, amount, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, type:path } );
+            let newEntry = new ReceiptVoucher( { userEmail, phoneNo, email, city, reference, amount, inputList, name,  memo, journalDate, journalNo, totalPaid, totalBalance, attachment, type:path } );
             await newEntry.save();
 
             res.status(200).json({ success: true, message: "Entry Added !" }) 
@@ -233,7 +233,7 @@ export default async function handler(req, res) {
 
         // Debit Note Invoice
         else if( path === 'DebitNote'){
-            const { phoneNo, email, city, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, city, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, path, importEntries, row } = req.body;
 
             for (const newItem of inputList) {
                 await PurchaseInvoice.findByIdAndUpdate(newItem.id, { $inc: { amountReceived: newItem.received } });
@@ -256,7 +256,7 @@ export default async function handler(req, res) {
                 }
             }
             
-            let newEntry = new DebitNote( { phoneNo, email, city, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, type:path } );
+            let newEntry = new DebitNote( { userEmail, phoneNo, email, city, fromAccount, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, type:path } );
             await newEntry.save();
 
             res.status(200).json({ success: true, message: "Entry Added !" })
@@ -265,7 +265,7 @@ export default async function handler(req, res) {
 
         // Credit Note Invoice
         else if( path === 'CreditNote'){
-            const { phoneNo, email, city, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, path, importEntries, row } = req.body;
+            const { userEmail, phoneNo, email, city, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, path, importEntries, row } = req.body;
 
             for (const newItem of inputList) {
                 await CreditSalesInvoice.findByIdAndUpdate(newItem.id, { $inc: { amountPaid: newItem.received } });
@@ -288,24 +288,20 @@ export default async function handler(req, res) {
                 }
             }
             
-            let newEntry = new CreditNote( { phoneNo, email, city, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, type:path } );
+            let newEntry = new CreditNote( { userEmail, phoneNo, email, city, paidBy, amount, dueDate, inputList, name,  memo, journalDate, journalNo, totalReceived, totalBalance, attachment, type:path } );
             await newEntry.save();
 
             res.status(200).json({ success: true, message: "Entry Added !" })
         }
 
         else if( path === 'clients'){
-            const { businessName, email, password, firstName, lastName } = req.body;
+            const { userEmail, businessName, email, password, firstName, lastName } = req.body;
             
-            let newEntry = new User( { businessName, email, password, firstName, lastName, } );
+            let newEntry = new User( { userEmail, businessName, email, password, firstName, lastName, } );
             await newEntry.save();
             res.status(200).json({ success: true, message: "Entry Added!" }) 
         }
         
-
-
-
-
 
         else{
             res.status(400).json({ success: false, message: "Internal Server Error !" }) 
