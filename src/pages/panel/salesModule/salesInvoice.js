5841a46efa5b7a2f7619882ca480d3febf4f0eb5
faixanmalik <faixan.malik554@gmail.com>
2023-09-23 13:15:36 +0500

    full controled inventory added

--- src/pages/panel/salesModule/salesInvoice.js
+++ src/pages/panel/salesModule/salesInvoice.js
@@ -363,6 +363,7 @@ import PaymentMethod from 'models/PaymentMethod';
       `}</style>
     <FullLayout>
 
+
     {/* React tostify */}
     <ToastContainer position="bottom-center" autoClose={1000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
 
